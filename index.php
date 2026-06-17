a83d3b1367daf511223aa7f9ecb938fae4865d82
JoaoItaloMP <mastedhd@gmail.com>
2026-06-16 23:40:54 -0300

    feat: implementar tentativa de letras

--- index.php
+++ index.php
@@ -9,9 +9,54 @@ if (!isset($_SESSION["palavra"])) {
     $_SESSION["palavra"] =
         $palavras[array_rand($palavras)];
 
+    $_SESSION["letrasCorretas"] = [];
 }
 
 $palavra = $_SESSION["palavra"];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $letra = strtoupper($_POST["letra"]);
+
+    if (
+        strlen($letra) === 1 &&
+        ctype_alpha($letra)
+    ) {
+
+        if (
+            !in_array(
+                $letra,
+                $_SESSION["letrasCorretas"]
+            )
+        ) {
+
+            if (str_contains($palavra, $letra)) {
+
+                $_SESSION["letrasCorretas"][] =
+                    $letra;
+            }
+        }
+    }
+}
+
+$exibicao = "";
+
+foreach (str_split($palavra) as $char) {
+
+    if (
+        in_array(
+            $char,
+            $_SESSION["letrasCorretas"]
+        )
+    ) {
+
+        $exibicao .= $char . " ";
+
+    } else {
+
+        $exibicao .= "_ ";
+    }
+}
 
 ?>
 
@@ -20,7 +65,6 @@ $palavra = $_SESSION["palavra"];
 <head>
     <meta charset="UTF-8">
     <title>Jogo da Forca PHP</title>
-
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -29,9 +73,21 @@ $palavra = $_SESSION["palavra"];
 
     <h1>Jogo da Forca PHP</h1>
 
-    <p>Palavra armazenada na sessão:</p>
+    <h2><?php echo $exibicao; ?></h2>
 
-    <h2><?php echo $palavra; ?></h2>
+    <form method="POST">
+
+        <input
+            type="text"
+            name="letra"
+            maxlength="1"
+            required>
+
+        <button type="submit">
+            Tentar
+        </button>
+
+    </form>
 
 </div>
 
